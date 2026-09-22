29b9eeb3b04f981f8f97d1e1133fdbe92a90c6b0
zuemimiag <zuemimiaung.ucsm@gmail.com>
2026-09-22 16:42:39 +0630

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
+import { getCustomers } from "./services/customerService";
+
 function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const loadCustomers = async () => {
+      const data = await getCustomers();
+      setCustomers(data);
+    };
+
+    loadCustomers();
+  }, []);
 
   return (
     <div>
       <h1>Smart Loan Management System</h1>
-      <p>Welcome to my loan management system.</p>
+
+      <h2>Customers</h2>
+
+      {customers.map((customer) => (
+        <div key={customer.id}>
+          <p>Name: {customer.customerName}</p>
+          <p>Phone: {customer.phone}</p>
+          <p>Address: {customer.address}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
-  
 }
 
 export default App;
